d580bfec85e272c7735c29a63e09dfde3a801984
Chris Jungerius <cjungerius@olbico.nl>
2017-06-08 17:21:53 +0200

    added build scripts with gulp

---
+++ gulpfile.js
@@ -0,0 +1,54 @@
+const gulp = require('gulp');
+var replace = require('gulp-replace');
+var gulpSequence = require('gulp-sequence');
+const shell = require('gulp-shell')
+
+var spawnSync = require('child_process').spawnSync;
+var del = require('del');
+
+gulp.task('clean', () => {
+    return del(['_build/']);
+});
+
+var gitVersionObj;
+gulp.task('getVersion', () => {
+
+    var cmd = spawnSync('gitversion', []);
+    gitVersionObj = JSON.parse(cmd.stdout.toString())
+    return;
+});
+
+gulp.task('copyTokenizedFiles', () => {
+
+    var packageVersion = gitVersionObj.MajorMinorPatch;
+    
+    return gulp.src([
+        './Task/task.json',
+        './vss-extension.json'], {base : "./" })
+        .pipe(replace("__VERSION__", packageVersion))
+        .pipe(replace("__VERSIONMAJOR__", gitVersionObj.Major))
+        .pipe(replace("__VERSIONMINOR__", gitVersionObj.Minor))
+        .pipe(replace("__VERSIONPATCH__", gitVersionObj.Patch))
+        .pipe(gulp.dest('./_build/'));
+});
+
+gulp.task('copyFiles', () => {
+    return gulp.src([
+            './Task/**/*.*',
+            './*.md',
+            './*.jpg',
+            './*.png'], {base:'./'})
+        .pipe(gulp.dest('./_build/'));
+ });
+
+gulp.task('createPackage', shell.task([
+    'tfx extension create --manifest-globs ./_build/vss-extension.json'
+]));
+
+gulp.task('publishPackage', shell.task([
+    'tfx extension publish'
+]));
+
+//gulp.task('publishbuild', gulpSequence('build', 'publishPackage'));
+gulp.task('build', gulpSequence('clean', ['getVersion', 'copyFiles'], ['copyTokenizedFiles'], 'createPackage'));
+gulp.task('default', ['build']);
